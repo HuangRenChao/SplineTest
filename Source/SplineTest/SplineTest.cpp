0d20b203f746cf789b17c0302b841ccfbf334376
dorgon <dorgonman@hotmail.com>
2016-10-26 11:24:33 +0800

    init

---
+++ Source/SplineTest/SplineTest.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SplineTest.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SplineTest, "SplineTest" );
---
+++ Source/SplineTest/SplineTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SplineTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLINETEST_API ASplineTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
